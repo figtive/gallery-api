813fcff398bba498ca5daef8ea5ddc856d722750
Danny August Ramaputra <daystram@gmail.com>
2021-11-16 14:14:14 +1000

    refactor: clean up dtos

--- dtos/project.go
+++ dtos/project.go
@@ -4,24 +4,24 @@ import "time"
 
 type Project struct {
 	ID          string    `json:"id"`
+	CourseID    string    `json:"courseId"`
 	Name        string    `json:"name"`
-	Active      bool      `json:"active"`
-	Description string    `json:"description"`
-	Field       string    `json:"field"`
-	Thumbnail   string    `json:"thumbnail"`
-	CreatedAt   time.Time `json:"createdAt"`
-	Metadata    string    `json:"metadata"`
 	Team        string    `json:"team"`
-	CourseId    string    `json:"courseId"`
+	Description string    `json:"description"`
+	Thumbnail   string    `json:"thumbnail"`
+	Field       string    `json:"field"`
+	Active      bool      `json:"active"`
+	Metadata    string    `json:"metadata"`
+	CreatedAt   time.Time `json:"createdAt"`
 }
 
 type ProjectInsert struct {
+	CourseID    string  `binding:"required" json:"courseId"`
 	Name        string  `binding:"required" json:"name"`
-	Active      bool    `binding:"required" json:"active"`
+	Team        string  `binding:"required" json:"team"`
 	Description string  `binding:"required" json:"description"`
 	Field       string  `binding:"required" json:"field"`
-	CourseID    string  `binding:"required" json:"courseId"`
-	Team        string  `binding:"required" json:"team"`
+	Active      bool    `binding:"required" json:"active"`
 	Metadata    *string `binding:"required" json:"metadata"`
 }
 
