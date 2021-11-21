aaaad6136f70037f539bce7303d10d23945cb61f
Danny August Ramaputra <daystram@gmail.com>
2021-11-21 19:56:55 +1000

    fix: incorrect join statement

--- models/blog.go
+++ models/blog.go
@@ -73,7 +73,7 @@ func (o *blogOrm) GetManyBookmarkByUserID(userID string) ([]Blog, error) {
 	var blogs []Blog
 	result := o.db.
 		Model(&Blog{}).
-		Joins("INNER JOIN blogs ON blogs.coursework_id = bookmarks.coursework_id").
+		Joins("INNER JOIN bookmarks ON blogs.coursework_id = bookmarks.coursework_id").
 		Where("bookmarks.user_id >= ?", userID).
 		Find(&blogs)
 	return blogs, result.Error
